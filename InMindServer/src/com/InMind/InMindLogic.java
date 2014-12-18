a3ed3189bb0e7b9e962e21c4c153b6bf7d2197c5
azariaa <azariaa@cs.cmu.edu>
2014-12-18 14:16:08 -0500

    Eliza and reflection

--- InMindServer/src/com/InMind/InMindLogic.java
+++ InMindServer/src/com/InMind/InMindLogic.java
@@ -1,6 +1,13 @@
 package com.InMind;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by User on 16-Dec-14.
@@ -19,11 +26,82 @@ public class InMindLogic {
     }
 
 
-    static public String getResponse(String userSentence)
+    // returns responses
+    // may have multiple (say something and launch something.
+    static public List<String> getResponse(String userSentence)
     {
-        String response = "";
+        List<String> response = new LinkedList<String>();
 
+
+        URI filePath = URI.create("file:///C:/InMind/git/Configurations/logic.csv");
+        String line = "";
+        String cvsSplitBy = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+        BufferedReader bufferedReader = null;
+
+        try{
+            bufferedReader = new BufferedReader(new FileReader(new File(filePath)));
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                // use comma as separator
+                String[] row = line.split(cvsSplitBy,-1);
+                Pattern p = Pattern.compile((row[0]), Pattern.CASE_INSENSITIVE);
+                Matcher m = p.matcher(userSentence);
+                if (m.matches())
+                {
+                    row[1] = row[1].replace("\"\"","\"");
+                    row[1] = row[1].replaceAll("^\"|\"$","");
+                    for (int i = 1; ; i++) {
+                        if (row[1].contains("%" + i)) {
+                            row[1] = row[1].replace("%" + i, m.group(i));
+                        } else if (row[1].contains("%r" + i)) //replace with reflection
+                        {
+                            row[1] = row[1].replace("%r" + i, reflect(m.group(i)));
+                        } else
+                            break;
+                    }
+                    response.add("Say^" + row[1]);
+                    if (!row[2].isEmpty())
+                        response.add(row[2]);
+                    break;
+                }
+
+            }
+
+        }catch (Exception ex) {
+            System.out.println("Logic: error in response");
+        }
         return response;
+    }
+
+    private static String reflect(String text) {
+
+        URI filePath = URI.create("file:///C:/InMind/git/Configurations/reflection.csv");
+        String line = "";
+        String cvsSplitBy = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+        BufferedReader bufferedReader = null;
+        String alreadyReplaced = "~";
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(new File(filePath)));
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                // use comma as separator
+                String[] row = line.split(cvsSplitBy, -1);
+                String wordsToMatch = "(?i)"+row[0]; //ignore case
+                String toMatch = "^"+wordsToMatch+" | "+wordsToMatch+" | "+wordsToMatch+"$|^"+wordsToMatch+"$";
+                String excludeAlreadyReplaced = toMatch+"(?=([^"+alreadyReplaced+"]*"+alreadyReplaced+"[^"+alreadyReplaced+"]*"+alreadyReplaced+")*[^"+alreadyReplaced+"]*$)";
+
+                text = text.replaceAll(excludeAlreadyReplaced,"~"+row[1]+"~");
+            }
+        } catch (Exception ex) {
+            System.out.println("Logic: error in reflection");
+            return text;
+        }
+
+        return text.replaceAll(alreadyReplaced," ").trim();
+
     }
 
 
@@ -44,9 +122,11 @@ public class InMindLogic {
 
                 System.out.println(asrRes.text);
 
-                String response = getResponse(asrRes.text);
+                List<String> response = getResponse(asrRes.text);
+                for(String command : response) {
 
-                tcpServer.sendMessage(response);//"Say^You Said:" + asrRes.text);
+                    tcpServer.sendMessage(command);//"Say^You Said:" + asrRes.text);
+                }
 
                 tcpServer.abandonClient();
 
